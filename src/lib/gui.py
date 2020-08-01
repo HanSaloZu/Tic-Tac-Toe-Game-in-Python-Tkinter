26328d641200d9bb67e44acbd5a656d30cfba9d6
JekaBB <conswintcomin@gmail.com>
2020-08-01 11:54:37 +0300

    Fix

--- src/lib/gui.py
+++ src/lib/gui.py
@@ -6,7 +6,7 @@ from .grid import Grid
 class Gui:
     window: object = Tk()
 
-    def __init__(self, title: str = "Tic-Tac-Toe-AI") -> None:
+    def __init__(self, title: str = "Tic-Tac-Toe") -> None:
         self.window.geometry("606x602")
         self.window.title(title)
 
